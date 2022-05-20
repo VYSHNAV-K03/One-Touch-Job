3ff3f7c44c052b00c79bf53c56f54d891627757c
vyshnav-chikku <vyshnavk891@gmail.com>
2022-05-21 00:36:17 +0530

    later commit 2

--- client/src/data/api.js
+++ client/src/data/api.js
@@ -4,6 +4,8 @@ import { useNavigate } from "react-router-dom";
 export const apiUrl = "https://onetouchjob-app.herokuapp.com/api";
 // export const apiUrl = "http://localhost:5000/api";
 
+// hello
+
 export const fileUpload = async (data, options, loginState) => {
   try {
     // console.log("sumesh");
